d607207781069811787d54b3f1b9fd7debaa76a7
Sergey Yanovich <ynvich@gmail.com>
2013-04-02 23:08:30 +0400

    print with timestamp on timer

--- pcs.c
+++ pcs.c
@@ -1,9 +1,23 @@
+#include <unistd.h>
 #include <stdio.h>
-int main()
+#include <time.h>
+
+void
+run(void)
 {
-	printf("Hello world!\n");
-#ifdef BFD64
-	printf("Hello files!\n");
-#endif
+	char buff[24];
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	strftime(&buff[0], sizeof(buff) - 1, "%b %e %H:%M:%S", &tm);
+	printf("%s Greetings, world!\n", buff);
+}
+
+int
+main(int argc, char **argv)
+{
+	while (1) {
+		run();
+		sleep(1);
+	}
 	return 0;
 }
