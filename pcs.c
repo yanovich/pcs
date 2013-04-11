9421db0173523a2429c13aa82dcf93c438f8a238
Sergey Yanovich <ynvich@gmail.com>
2013-04-11 22:51:54 +0400

    working HW correction with delta

--- pcs.c
+++ pcs.c
@@ -189,6 +189,51 @@ close_fd:
 	return err;
 }
 
+int set_parallel_output_status(unsigned int slot, unsigned  status)
+{
+	int fd, err;
+	struct termios options;
+	char buff[256];
+	char *p;
+	int i = 0;
+
+	if (slot == 0 || slot > 8) {
+		printf("%s %u: bad slot (%u)\n", __FILE__, __LINE__, slot);
+		return -1;
+	}
+	err = snprintf(&buff[0], sizeof(buff) - 1,
+		       	"/sys/bus/icpdas/devices/slot%02u/output_status", slot);
+	if (err >= sizeof(buff)) {
+		printf("%s %u: %s (%i)\n", __FILE__, __LINE__, strerror(errno),
+			       	errno);
+		return -1;
+	}
+	fd = open(buff, O_RDWR);
+	if (-1 == fd) {
+		printf("%s %u: %s (%i)\n", __FILE__, __LINE__, strerror(errno),
+			       	errno);
+		return -1;
+	}
+
+	err = snprintf(&buff[0], sizeof(buff) - 1, "0x%08x", status);
+	if (err >= sizeof(buff)) {
+		printf("%s %u: %s (%i)\n", __FILE__, __LINE__, strerror(errno),
+			       	errno);
+		return -1;
+	}
+	err = write(fd, buff, strlen(buff));
+	if(0 > err) {
+		printf("%s %u: %s (%i)\n", __FILE__, __LINE__,
+				strerror(errno), errno);
+		goto close_fd;
+	}
+
+	err = 0;
+close_fd:
+	close(fd);
+	return err;
+}
+
 int ni1000(int ohm)
 {
 	const int o[] = {7909, 8308, 8717, 9135 , 9562 , 10000 ,
@@ -294,11 +339,12 @@ struct site_status {
 	int		t21;
 	unsigned int	p11;
 	unsigned int	p12;
+	unsigned int	do0;
 	int		v11;
 	int		v21;
-	int		t11_sp;
-	int		t12_sp;
-	int		t21_sp;
+	int		e11;
+	int		e12;
+	int		e21;
 };
 
 int load_site_status(struct site_status *site_status)
@@ -348,16 +394,10 @@ int load_site_status(struct site_status *site_status)
 		return -1;
 	}
 
+	site_status->do0 = output;
 	site_status->v11 = 0;
 	site_status->v21 = 0;
-	if (output & 0x20)
-		site_status->v11 = 1;
-	if (output & 0x10)
-		site_status->v11 = -1;
-	if (output & 0x40)
-		site_status->v21 = -1;
-	if (output & 0x80)
-		site_status->v21 = 1;
+
 	return 0;
 };
 
@@ -414,12 +454,24 @@ unsigned Zh(int a, int b, int c, int x)
 	return (0x10000 * (c - x)) / (c - b);
 }
 
-int calculate_action(struct site_status *site_status)
+int calculate_action(struct site_status *curr, struct site_status *prev)
 {
-	int e21 = site_status->t21 - 590;
+	int e21 = curr->t21 - 570;
+	int d21 = e21 - prev->e21;
 	struct fuzzy_result res = {0};
 	unsigned h;
 
+	curr->e21 = e21;
+
+	h =  Zh(-1000,-50,  -30, e21);
+	Dm(  400, 5000, 5000, h, &res);
+	printf("T21 IS   BN: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
+	h =  Dh( -50, -30,  -10, e21);
+	Dm(  100, 400, 700, h, &res);
+	printf("T21 IS    N: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
+	h =  Dh( -30, -10,    0, e21);
+	Dm(    0, 100,  200, h, &res);
+	printf("T21 IS   SN: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
 	h =  Dh( -10,   0,   30, e21);
 	Dm( -300,   0,  300, h, &res);
 	printf("T21 IS Zero: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
@@ -432,49 +484,88 @@ int calculate_action(struct site_status *site_status)
 	h =  Sh(  80, 180, 1000, e21);
 	Dm(-5000, -5000, -400, h, &res);
 	printf("T21 IS   BP: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
-	h =  Dh( -30, -10,    0, e21);
-	Dm(    0, 100,  200, h, &res);
-	printf("T21 IS   SN: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
-	h =  Dh( -50, -30,  -10, e21);
+	h =  Zh(-1000, -10,   -1, d21);
 	Dm(  100, 400, 700, h, &res);
-	printf("T21 IS    N: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
-	h =  Zh(-1000,-50,  -30, e21);
-	Dm(  400, 5000, 5000, h, &res);
-	printf("T21 IS   BN: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
-	site_status->t21_sp = e21;
+	printf("D21 IS    N: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
+	h =  Sh(     1, 10, 1000, d21);
+	Dm( -700, -400, -100, h, &res);
+	printf("D21 IS    P: 0x%05x, action: %5i, mass: %05x\n", h, res.value, res.mass);
+	curr->v21 = res.value;
 	return 0;
 };
 
-void
-run(void)
+unsigned
+execute_v21(struct site_status *curr)
 {
-	struct site_status site_status = {0};
+	unsigned usec;
+	if (-50 < curr->v21 && curr->v21 < 50)
+		return 0;
 
-	load_site_status(&site_status);
+	curr->do0 &= ~0xc0;
+	if (curr->v21 < 0) {
+		usec = curr->v21 * -1000;
+		curr->do0 |=  0x80;
+	} else {
+		usec = curr->v21 *  1000;
+		curr->do0 |=  0x40;
+	}
+	if (usec > 5000000)
+		usec = 5000000;
+
+	set_parallel_output_status(1, curr->do0);
+
+	usleep(usec);
+
+	curr->do0 &= ~0xc0;
+	set_parallel_output_status(1, curr->do0);
+
+	return 0;
+}
+
+void
+log_status(struct site_status *site_status)
+{
 	syslog(LOG_INFO, "T %3i, T11 %3i, T12 %3i, T21 %3i, P11 %3u, P12 %3u, "
-			"V11 %2i, V12 %2i\n", site_status.t,
-		       	site_status.t11, site_status.t12, site_status.t21,
-		       	site_status.p11, site_status.p12, site_status.v11,
-		       	site_status.v21);
+			"V11 %2i, V21 %2i\n", site_status->t,
+		       	site_status->t11, site_status->t12, site_status->t21,
+		       	site_status->p11, site_status->p12, site_status->v11,
+		       	site_status->v21);
 	printf("T %3i, T11 %2i, T12 %2i, T21 %2i, P11 %3u, P12 %3u, "
-			"V11 %2i, V12 %2i\n", site_status.t,
-		       	site_status.t11, site_status.t12, site_status.t21,
-		       	site_status.p11, site_status.p12, site_status.v11,
-		       	site_status.v21);
-	calculate_action(&site_status);
-	printf("setp:  T11 %3i, T12 %3i, T21 %3i"
+			"V11 %2i, V21 %2i\n", site_status->t,
+		       	site_status->t11, site_status->t12, site_status->t21,
+		       	site_status->p11, site_status->p12, site_status->v11,
+		       	site_status->v21);
+	printf("error: T11 %3i, T12 %3i, T21 %3i"
 			"\n",
-		       	site_status.t11_sp, site_status.t12_sp,
-		       	site_status.t21_sp);
+		       	site_status->e11, site_status->e12,
+		       	site_status->e21);
 }
 
 int
 main(int argc, char **argv)
 {
+	unsigned c = 0;
+	unsigned t;
+	struct site_status s1 = {0}, s2 = {0};
+	struct site_status *curr, *prev;
+
 	openlog("pcs", 0, LOG_DAEMON);
+	load_site_status(&s2);
 	while (1) {
-		run();
-		sleep(5);
+		c = c ^ 1;
+		if (c) {
+			curr = &s1;
+			prev = &s2;
+		} else {
+			curr = &s2;
+			prev = &s1;
+		}
+		load_site_status(curr);
+		calculate_action(curr, prev);
+		log_status(curr);
+		t = 10000000;
+		t -= execute_v21(curr);;
+		usleep(t);
 	}
 	return 0;
 }
