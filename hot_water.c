531ab039124557e58806b6ee60a5c185b406a6f3
Sergey Yanovich <ynvich@gmail.com>
2013-05-07 01:36:34 +0400

    ref: rename variable

--- hot_water.c
+++ hot_water.c
@@ -43,24 +43,24 @@ struct hot_water_config {
 static void
 hot_water_run(struct site_status *curr, void *conf)
 {
-	struct hot_water_config *hwc = conf;
+	struct hot_water_config *c = conf;
 	int vars[2];
 	int v21;
 
 	debug("running hot water\n");
-	if (hwc->first_run) {
-		hwc->first_run = 0;
-		hwc->t21_prev = curr->t21;
+	if (c->first_run) {
+		c->first_run = 0;
+		c->t21_prev = curr->t21;
 	}
 	vars[0] = curr->t21 - 570;
-	vars[1] = curr->t21 - hwc->t21_prev;
-	hwc->t21_prev = curr->t21;
+	vars[1] = curr->t21 - c->t21_prev;
+	c->t21_prev = curr->t21;
 
-	v21 = process_fuzzy(&hwc->fuzzy, &vars[0]);
-	if (!hwc->valve || !hwc->valve->ops || !hwc->valve->ops->adjust)
+	v21 = process_fuzzy(&c->fuzzy, &vars[0]);
+	if (!c->valve || !c->valve->ops || !c->valve->ops->adjust)
 		fatal("bad valve\n");
 	
-	hwc->valve->ops->adjust(v21, hwc->valve->data);
+	c->valve->ops->adjust(v21, c->valve->data);
 	return;
 }
 
@@ -68,7 +68,7 @@ static int
 hot_water_log(struct site_status *curr, void *conf, char *buff,
 		const int sz, int o)
 {
-	struct hot_water_config *hwc = conf;
+	struct hot_water_config *c = conf;
 	int b = 0;
 
 	if (o == sz)
@@ -78,8 +78,8 @@ hot_water_log(struct site_status *curr, void *conf, char *buff,
 		b++;
 	}
 	b += snprintf(&buff[o + b], sz - o - b, "T21 %3i ", curr->t21);
-	if (hwc->valve && hwc->valve->ops && hwc->valve->ops->log)
-		b += hwc->valve->ops->log(hwc->valve->data, &buff[o + b],
+	if (c->valve && c->valve->ops && c->valve->ops->log)
+		b += c->valve->ops->log(c->valve->data, &buff[o + b],
 			       	sz, o + b);
 
 	return b;
@@ -94,41 +94,41 @@ void
 load_hot_water(struct list_head *list)
 {
 	struct process *hwp = (void *) xmalloc (sizeof(*hwp));
-	struct hot_water_config *hwc = (void *) xmalloc (sizeof(*hwc));
+	struct hot_water_config *c = (void *) xmalloc (sizeof(*c));
 	struct fuzzy_clause *fcl;
 
-	INIT_LIST_HEAD(&hwc->fuzzy);
+	INIT_LIST_HEAD(&c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 2, -1500, -50,  -30, 0,   400,  7000, 7000);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 0,   -50, -30,  -10, 0,   100,   400,  700);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 0,   -30, -10,    0, 0,     0,   100,  200);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 0,   -10,   0,   30, 0,  -300,     0,  300);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 0,     0,  30,   80, 0,  -200,  -100,    0);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 0,    30,  80,  180, 0,  -700,  -400, -100);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 0, 1,    80, 180, 1500, 0, -7000, -7000, -400);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 1, 2, -1000, -10,   -1, 0,   100,   400,  700);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 	fcl = (void *) xmalloc(sizeof(*fcl));
 	fuzzy_clause_init(fcl, 1, 1,     1,  10, 1000, 0,  -700,  -400, -100);
-	list_add_tail(&fcl->fuzzy_entry, &hwc->fuzzy);
+	list_add_tail(&fcl->fuzzy_entry, &c->fuzzy);
 
-	hwc->first_run = 1;
-	hwc->valve = load_2way_valve(50, 5000, 47000, 8, 7);
-	hwp->config = (void *) hwc;
+	c->first_run = 1;
+	c->valve = load_2way_valve(50, 5000, 47000, 8, 7);
+	hwp->config = (void *) c;
 	hwp->ops = &hot_water_ops;
 	list_add_tail(&hwp->process_entry, list);
 }
