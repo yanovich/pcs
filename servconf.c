30ef350af2d3872b43f59c1c1dd9017276428dfe
Sergey Yanovich <ynvich@gmail.com>
2013-05-07 21:27:31 +0400

    add servconf.c to libpcs.a

--- servconf.c
+++ servconf.c
@@ -1,8 +1,27 @@
+/* servconf.c -- load configuration from a YAML file
+ * Copyright (C) 2013 Sergey Yanovich <ynvich@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <yaml.h>
 
-#include <log.h>
+#include "log.h"
 
 #include "includes.h"
 #include "list.h"
@@ -163,12 +182,4 @@ load_server_config(const char *filename, struct server_config *conf)
 
 	yaml_parser_delete(&parser);
 	fclose(f);
-}
-
-int
-main()
-{
-	log_init(PACKAGE_NAME, LOG_NOTICE, LOG_DAEMON, 1);
-	load_server_config("test.conf", NULL);
-	return 0;
 }
