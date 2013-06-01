5a4f5856747213bafac538e3af19b8e616f1da45
Sergey Yanovich <ynvich@gmail.com>
2013-06-02 01:04:34 +0400

    fc: extract fc_serial_exchange()

--- fc-raw.c
+++ fc-raw.c
@@ -214,35 +214,52 @@ static void usage(int ret)
 	exit(ret);
 }
 
-static void
-fc_status(unsigned long control)
+struct fc_session {
+	unsigned long		control;
+	unsigned int		param;
+	unsigned int		index;
+	union {
+		unsigned long	val;
+		char		text[64];
+	};
+	unsigned int			do_write;
+};
+
+static int
+fc_status(struct fc_session *c, struct fc_session *r)
 {
 	int err;
 	char data[MAX_RESPONSE];
 	char cmd[MAX_RESPONSE];
+	int i;
 
 	cmd[0] = 0x02;
 	cmd[1] =    6;
 	cmd[2] = 0x01;
 	/* PCD1 */
-	cmd[3] = (control >> 24) & 0xff;
-	cmd[4] = (control >> 16) & 0xff;
+	cmd[3] = (c->control >> 24) & 0xff;
+	cmd[4] = (c->control >> 16) & 0xff;
 	/* PCD2 */
-	cmd[5] = (control >> 8) & 0xff;
-	cmd[6] = control & 0xff;
+	cmd[5] = (c->control >> 8) & 0xff;
+	cmd[6] = c->control & 0xff;
 	/* BCC */
 	cmd[7] = 0x00;
 
 	err = fc_raw_serial_exchange(cmd, data);
 	if (err < 4) {
 		error ("fc: exchage failed\n");
-		return;
+		return -1;
 	}
+	r->control = 0;
+	for (i = 3; i < 7; i++) {
+		r->control <<= 8;
+		r->control |= data[i];
+	}
+	return 0;
 }
 
-static void
-fc_param(unsigned long control, unsigned long param, unsigned long index,
-		unsigned long val, int do_write)
+static int
+fc_param(struct fc_session *c, struct fc_session *r)
 {
 	int err;
 	int i = 0;
@@ -250,8 +267,8 @@ fc_param(unsigned long control, unsigned long param, unsigned long index,
 	char cmd[MAX_RESPONSE];
 	unsigned flag = 0x10;
 
-	if (do_write) {
-		switch (fc_type[param]) {
+	if (c->do_write) {
+		switch (fc_type[c->param]) {
 		case 3:
 		case 5:
 		case 6:
@@ -269,36 +286,65 @@ fc_param(unsigned long control, unsigned long param, unsigned long index,
 	cmd[i++] =   14;
 	cmd[i++] = 0x01;
 	/* PKE */
-	cmd[i++] = flag | ((param >> 8) & 0x3f);
-	cmd[i++] = param & 0xff;
+	cmd[i++] = flag | ((c->param >> 8) & 0xf);
+	cmd[i++] = c->param & 0xff;
 	/* IND */
-	cmd[i++] = (index >> 8) & 0xff;
-	cmd[i++] = index & 0xff;
+	cmd[i++] = (c->index >> 8) & 0xff;
+	cmd[i++] = c->index & 0xff;
 	/* PWE1 */
-	cmd[i++] = (val >> 24) & 0xff;
-	cmd[i++] = (val >> 16) & 0xff;
+	cmd[i++] = (c->val >> 24) & 0xff;
+	cmd[i++] = (c->val >> 16) & 0xff;
 	/* PWE2 */
-	cmd[i++] = (val >> 8) & 0xff;
-	cmd[i++] = val & 0xff;
+	cmd[i++] = (c->val >> 8) & 0xff;
+	cmd[i++] = c->val & 0xff;
 	/* PCD1 */
-	cmd[i++] = (control >> 24) & 0xff;
-	cmd[i++] = (control >> 16) & 0xff;
+	cmd[i++] = (c->control >> 24) & 0xff;
+	cmd[i++] = (c->control >> 16) & 0xff;
 	/* PCD2 */
-	cmd[i++] = (control >> 8) & 0xff;
-	cmd[i++] = control & 0xff;
+	cmd[i++] = (c->control >> 8) & 0xff;
+	cmd[i++] = c->control & 0xff;
 	/* BCC */
 	cmd[i++] = 0x00;
 
 	err = fc_raw_serial_exchange(cmd, data);
 	if (err < 4) {
 		error ("fc: exchage failed\n");
-		return;
+		return -1;
 	}
+	r->val = 0;
+	for (i = 7; i < 11; i++) {
+		r->val <<= 8;
+		r->val |= data[i];
+	}
+	r->control = 0;
+	for (; i < 15; i++) {
+		r->control <<= 8;
+		r->control |= data[i];
+	}
+	r->index = data[6];
+	r->param = data[4];
+	r->param |= (data[3] & 0xf) << 8;
+	flag = data[3] >> 4;
+	switch (fc_type[c->param]) {
+	case 3:
+	case 5:
+	case 6:
+		if (flag != 1)
+			return -1;
+		break;
+	case 4:
+	case 7:
+		if (flag != 2)
+			return -1;
+		break;
+	default:
+		return -1;
+	};
+	return 0;
 }
 
-static void
-fc_text(unsigned long control, unsigned long param, unsigned long index,
-		char *val, int do_write)
+static int
+fc_text(struct fc_session *c, struct fc_session *r)
 {
 	int err;
 	int i = 0;
@@ -306,95 +352,131 @@ fc_text(unsigned long control, unsigned long param, unsigned long index,
 	int n;
 	char data[MAX_RESPONSE];
 	char cmd[MAX_RESPONSE];
+	unsigned long index = c->index;
+	char *text;
+	unsigned flag;
 
-	if (do_write) {
+	if (c->do_write) {
 		index += 0x500;
-		n = strlen(val);
+		n = strlen(c->text);
+		text = c->text;
 	} else {
 		index += 0x400;
 		n = 4;
-		val = (char *) &index;
+		text = (char *) &index;
 	}
 
 	cmd[i++] = 0x02;
 	cmd[i++] = 10 + n;
 	cmd[i++] = 0x01;
 	/* PKE */
-	cmd[i++] = 0xf0 | ((param >> 8) & 0x3f);
-	cmd[i++] = param & 0xff;
+	cmd[i++] = 0xf0 | ((c->param >> 8) & 0xf);
+	cmd[i++] = c->param & 0xff;
 	/* IND */
 	cmd[i++] = (index >> 8) & 0xff;
 	cmd[i++] = index & 0xff;
 	index = 0;
 	/* PWE */
 	while (j < n)
-		cmd[i++] = val[j++];
+		cmd[i++] = text[j++];
 	/* PCD1 */
-	cmd[i++] = (control >> 24) & 0xff;
-	cmd[i++] = (control >> 16) & 0xff;
+	cmd[i++] = (c->control >> 24) & 0xff;
+	cmd[i++] = (c->control >> 16) & 0xff;
 	/* PCD2 */
-	cmd[i++] = (control >> 8) & 0xff;
-	cmd[i++] = control & 0xff;
+	cmd[i++] = (c->control >> 8) & 0xff;
+	cmd[i++] = c->control & 0xff;
 	/* BCC */
 	cmd[i] = 0x00;
 
 	err = fc_raw_serial_exchange(cmd, data);
 	if (err < 4) {
 		error ("fc: exchage failed\n");
-		return;
+		return -1;
 	}
+	n = data[1] - 10;
+	for (i = 7, j = 0; j < n; i++, j++)
+		r->text[j] = data[i];
+	r->control = 0;
+	n = i + 4;
+	for (; i < n; i++) {
+		r->control <<= 8;
+		r->control |= data[i];
+	}
+	r->index = data[6];
+	r->param = data[4];
+	r->param |= (data[3] & 0xf) << 8;
+	flag = data[3] >> 4;
+	if (flag != 0xf)
+		return -1;
+	return 0;
+}
+
+int
+fc_serial_exchange(struct fc_session *cmd, struct fc_session *resp)
+{
+	int err;
+
+	if (cmd->param && fc_type[cmd->param] == 9) {
+		err = fc_text(cmd, resp);
+	} else if (cmd->param) {
+		err = fc_param(cmd, resp);
+	} else {
+		err = fc_status(cmd, resp);
+	}
+	return err;
 }
 
 int main(int ac, char *av[] )
 {
 	int opt;
 	char *bad;
-	unsigned long control = 0x0;
-	unsigned long param = 0;
-	unsigned long index = 0;
-	unsigned long val = 0;
-	char *text = NULL;
-	int do_write = 0;
+	struct fc_session cmd = {0};
+	struct fc_session resp = {0};
 	int log_level = LOG_NOTICE;
 
 	while ((opt = getopt(ac, av, "c:p:i:w:dh")) != -1) {
 		switch (opt) {
                 case 'c': 
-			control = strtoul(optarg, &bad, 0);
+			cmd.control = strtoul(optarg, &bad, 0);
 			if (bad[0] != 0)
 				usage(1);
 			break;
 		case 'p':
-			if (param != 0)
+			if (cmd.param != 0)
 				usage(1);
-			param = strtoul(optarg, &bad, 0);
+			cmd.param = strtoul(optarg, &bad, 0);
 			if (bad[0] != 0)
 				usage(1);
-			if (param == 0)
+			if (cmd.param == 0)
 				usage(1);
 			break;
 		case 'i':
-			if (param == 0)
+			if (cmd.param == 0)
 				usage(1);
-			index = strtoul(optarg, &bad, 0);
+			cmd.index = strtoul(optarg, &bad, 0);
 			if (bad[0] != 0)
 				usage(1);
 			break;
 		case 'w':
-			if (param == 0)
+			if (cmd.param == 0)
 				usage(1);
-			if (fc_type[param] == 3 || fc_type[param] == 4)
-				val = (unsigned long) strtol(optarg, &bad, 0);
-			else if (fc_type[param] >= 5 || fc_type[param] <= 7)
-				val = strtoul(optarg, &bad, 0);
-			else if (fc_type[param] == 9)
-				text = optarg;
-			else
-				fatal("unsupported param type %i(%lu)\n",
-					       fc_type[param], param);
+			if (fc_type[cmd.param] == 3
+				       	|| fc_type[cmd.param] == 4)
+				cmd.val = (unsigned long)
+				       	strtol(optarg, &bad, 0);
+			else if (fc_type[cmd.param] >= 5
+				       	&& fc_type[cmd.param] <= 7)
+				cmd.val = strtoul(optarg, &bad, 0);
+			else if (fc_type[cmd.param] == 9) {
+				if (strlen(optarg) > 63)
+					fatal("text is too long\n");
+				strcpy(cmd.text, optarg);
+			} else
+				fatal("unsupported param type %i(%u)\n",
+					       fc_type[cmd.param], cmd.param);
 			if (bad[0] != 0)
 				usage(1);
-			do_write = 1;
+			cmd.do_write = 1;
 			break;
 		case 'd':
 			if (log_level >= LOG_DEBUG)
@@ -412,12 +494,38 @@ int main(int ac, char *av[] )
 	}
 
 	log_init(av[0], log_level, LOG_DAEMON, 1);
-	if (param && fc_type[param] == 9)
-		fc_text(control, param, index, text, do_write);
-	else if (param)
-		fc_param(control, param, index, val, do_write);
-	else
-		fc_status(control);
+	if (fc_serial_exchange(&cmd, &resp) < 0)
+		return -1;
+	printf("Status: 0x%04lx\n", (resp.control >> 16) & 0xffff);
+
+	switch(fc_type[resp.param]) {
+	case 3:
+		printf("%u-%u[%u]: %i\n", resp.param / 100, resp.param % 100,
+				resp.index, (int) (resp.val & 0xffff));
+		break;
+	case 4:
+		printf("%u-%u[%u]: %li\n", resp.param / 100, resp.param % 100,
+				resp.index, (long) (resp.val));
+		break;
+	case 5:
+		printf("%u-%u[%u]: %lu\n", resp.param / 100, resp.param % 100,
+				resp.index, resp.val & 0xff);
+		break;
+	case 6:
+		printf("%u-%u[%u]: %lu\n", resp.param / 100, resp.param % 100,
+				resp.index, resp.val & 0xffff);
+		break;
+	case 7:
+		printf("%u-%u[%u]: %lu\n", resp.param / 100, resp.param % 100,
+				resp.index, resp.val);
+		break;
+	case 9:
+		printf("%u-%u[%u]: %s\n", resp.param / 100, resp.param % 100,
+				resp.index, resp.text);
+		break;
+	case 0:
+		break;
+	}
 
 	return 0;
 }
