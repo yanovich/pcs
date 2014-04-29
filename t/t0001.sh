c6125aa26eff37de51bd456d4cecf271e95d5030
Sergei Ianovich <ynvich@gmail.com>
2014-04-29 16:42:21 +0400

    test for mark block output
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

---
+++ t/t0001.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+SELF=`basename $0`
+coproc ./pcs -f t/$SELF.conf 2>/tmp/$SELF.log &&
+sleep 1.5 && kill $COPROC_PID &&
+test 2 -eq `grep -e "mark1:" /tmp/$SELF.log | wc -l` &&
+test 2 -eq `cat /tmp/$SELF.log | wc -l`
