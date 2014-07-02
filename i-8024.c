c34ce4ab6e67ac729c6b2b45b5362f968b3c92e8
Sergei Ianovich <ynvich@gmail.com>
2014-07-03 06:08:03 +1000

    support I-8024 analog output
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

---
+++ i-8024.c
@@ -0,0 +1,113 @@
+/* i-8024.c -- write I-8024 status
+ * Copyright (C) 2014 Sergei Ianovich <ynvich@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "includes.h"
+
+#include "block.h"
+#include "i-8024.h"
+#include "icpdas.h"
+#include "map.h"
+#include "state.h"
+
+#define PCS_BLOCK	"i-8024"
+
+struct i_8024_out_state {
+	unsigned		slot;
+	long			*AO[4];
+};
+
+static void
+i_8024_out_run(struct block *b, struct server_state *s)
+{
+	struct i_8024_out_state *d = b->data;
+	int err, i;
+
+	for (i = 0; i < 4; i++) {
+		if (NULL == d->AO[i])
+			continue;
+		err = icpdas_set_parallel_analog_output(d->slot, i, *d->AO[i]);
+		if (0 > err)
+			error("%s: i-8024 output error %i\n", b->name, err);
+	}
+}
+
+static int
+set_out_slot(void *data, long value)
+{
+	struct i_8024_out_state *d = data;
+	d->slot = (unsigned) value;
+	debug("slot = %u\n", d->slot);
+	return 0;
+}
+
+static void
+set_input(void *data, const char const *key, long *input)
+{
+	struct i_8024_out_state *d = data;
+	char *bad;
+	long i = strtol(key, &bad, 10);
+	if (bad[0] != 0 || i < 0 || i >= 4)
+		fatal("i-8024 out: bad input '%s'\n", key);
+	d->AO[i] = input;
+}
+
+static struct pcs_map out_setpoints[] = {
+	{
+		.key			= "slot",
+		.value			= set_out_slot,
+	}
+	,{
+	}
+};
+
+static struct pcs_map out_inputs[] = {
+	{
+		.key			= NULL,
+		.value			= set_input,
+	}
+};
+
+static void *
+i_8024_out_alloc(void)
+{
+	return xzalloc(sizeof(struct i_8024_out_state));
+}
+
+static struct block_ops i_8024_out_ops = {
+	.run		= i_8024_out_run,
+};
+
+static struct block_ops *
+i_8024_out_init(void *data)
+{
+	return &i_8024_out_ops;
+}
+
+static struct block_builder i_8024_out_builder = {
+	.alloc		= i_8024_out_alloc,
+	.ops		= i_8024_out_init,
+	.setpoints	= out_setpoints,
+	.inputs		= out_inputs,
+};
+
+struct block_builder *
+load_i_8024_builder(void)
+{
+	return &i_8024_out_builder;
+}
