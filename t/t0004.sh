4dc4a3a0d57abcf063ce15971da918c82293bd57
Sergei Ianovich <ynvich@gmail.com>
2014-07-02 22:00:03 +1000

    fix t0004 timing
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- t/t0004.sh
+++ t/t0004.sh
@@ -2,7 +2,7 @@
 SELF=`basename $0`
 ./pcs -tf t/$SELF.conf &&
 coproc ./pcs -Df t/$SELF.conf 2>/tmp/$SELF.log &&
-sleep 0.15 &&
+sleep 0.125 &&
 kill $COPROC_PID &&
 test 2 -eq `grep -e "mark1:1 mark2:1" /tmp/$SELF.log | wc -l` &&
 test 3 -eq `grep -e "mark3:1" /tmp/$SELF.log | wc -l` &&
