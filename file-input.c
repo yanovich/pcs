27551a215bc4fb6fba275ad8f38a62f0737ce423
Sergei Ianovich <ynvich@gmail.com>
2014-07-08 00:45:20 +1000

    file input: separate files for input, cache and initialization
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- file-input.c
+++ file-input.c
@@ -34,8 +34,11 @@
 struct file_input_state {
 	long			count;
 	const char		*path;
+	const char		*cache_path;
+	const char		*init_path;
 	struct list_head	key_list;
 	long			*cache;
+	int			first;
 };
 
 struct line_key {
@@ -43,6 +46,7 @@ struct line_key {
 	const char		*key;
 	int			i;
 	int			present;
+	int			update;
 };
 
 static struct block_builder builder;
@@ -75,7 +79,9 @@ parse_value(struct pcs_parser_node *node, yaml_event_t *event)
 			return 0;
 		d->cache[k] = val;
 		c->present = 1;
-		debug(" %s: %s\n", key, value);
+		if (b->outputs[k] != val)
+			c->update = 1;
+		debug(" %s: %s%s\n", key, value, c->update ? "*" : "");
 	}
 	pcs_parser_remove_node(node);
 	return 1;
@@ -97,15 +103,35 @@ static struct pcs_parser_map stream_map = {
 	.data			= &document_map,
 };
 
+static void
+save_file(struct block *b)
+{
+	struct file_input_state *d = b->data;
+	struct line_key *c;
+	FILE *f;
+
+        f = fopen(d->cache_path, "w");
+	if (!f) {
+		error("%s: failed to open '%s'\n", PCS_BLOCK, d->path);
+		return;
+	}
+	list_for_each_entry(c, &d->key_list, key_entry) {
+		fprintf(f, "%s: %li\n", c->key, d->cache[c->i]);
+	}
+	fclose(f);
+}
+
 static int
 load_file(struct block *b, const char const *filename)
 {
 	struct file_input_state *d = b->data;
 	struct line_key *c;
 	int err;
+	int update = 0;
 
 	list_for_each_entry(c, &d->key_list, key_entry) {
 		c->present = 0;
+		c->update = 0;
 	}
 	err = pcs_parse_yaml(filename, &stream_map, b);
 	if (err)
@@ -113,8 +139,13 @@ load_file(struct block *b, const char const *filename)
 	list_for_each_entry(c, &d->key_list, key_entry) {
 		if (1 != c->present)
 			return 1;
+		if (c->update)
+			update = 1;
 	}
 	memcpy(b->outputs, d->cache, sizeof(*d->cache) * d->count);
+	if (0 == d->first && d->cache_path && update)
+		save_file(b);
+
 	return 0;
 }
 
@@ -122,6 +153,14 @@ static void
 file_input_run(struct block *b, struct server_state *s)
 {
 	struct file_input_state *d = b->data;
+	int err = 1;
+	if (d->first) {
+		if (d->cache_path)
+			err = load_file(b, d->cache_path);
+		if (err && d->init_path)
+			err = load_file(b, d->init_path);
+	}
+	d->first = 0;
 	b->outputs[d->count] = load_file(b, d->path);
 }
 
@@ -151,6 +190,32 @@ set_path(void *data, const char const *key, const char const *value)
 	return 0;
 }
 
+static int
+set_cache_path(void *data, const char const *key, const char const *value)
+{
+	struct file_input_state *d = data;
+	if (d->cache_path) {
+		error("%s: 'cache_path' already initialized\n", PCS_BLOCK);
+		return 1;
+	}
+	d->cache_path = strdup(value);
+	debug("cache_path = %s\n", d->cache_path);
+	return 0;
+}
+
+static int
+set_init_path(void *data, const char const *key, const char const *value)
+{
+	struct file_input_state *d = data;
+	if (d->init_path) {
+		error("%s: 'init_path' already initialized\n", PCS_BLOCK);
+		return 1;
+	}
+	d->init_path = strdup(value);
+	debug("init_path = %s\n", d->init_path);
+	return 0;
+}
+
 static struct pcs_map strings[] = {
 	{
 		.key			= "key",
@@ -161,6 +226,14 @@ static struct pcs_map strings[] = {
 		.value			= set_path,
 	}
 	,{
+		.key			= "cache path",
+		.value			= set_cache_path,
+	}
+	,{
+		.key			= "init path",
+		.value			= set_init_path,
+	}
+	,{
 	}
 };
 
@@ -169,6 +242,7 @@ alloc(void)
 {
 	struct file_input_state *d = xzalloc(sizeof(struct file_input_state));
 	INIT_LIST_HEAD(&d->key_list);
+	d->first = 1;
 	return d;
 }
 
